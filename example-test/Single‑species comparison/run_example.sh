44d72e16e73b34fbf80b5debea8287f69bfddf08
yogurtYH <126230802+yogurtYH@users.noreply.github.com>
2026-03-12 15:50:13 +0800

    Add files via upload

---
+++ example-test/Single‑species comparison/run_example.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+# GeneViz 示例：比较 LOC_Os06g50440 和 LOC_Os06g50789
+
+python ./GeneViz.py \
+    --gene1 LOC_Os06g50440 \
+    --gene2 LOC_Os06g50789 \
+    --gff gene_subset.gff \
+    --fasta genome_subset.fa \
+    --te_gff te_subset.gff \
+    --auto_complementary \
+    --output example_output \
+    --bezier \
+    --extend 5000
